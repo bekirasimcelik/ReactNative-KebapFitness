7cb8d5eba53c287e1c1996b071426cdfc6748714
bekirasimcelik <bekirasimcelik@hotmail.com>
2024-03-31 03:23:42 +0300

    Transition Between Pages

---
+++ src/pages/First.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {Button, SafeAreaView, StyleSheet, Text} from 'react-native';
+
+function First({navigation}) {
+  function navigateToPage() {
+    navigation.navigate('SecondScreen');
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Hello First</Text>
+      <Button title="Go to Second" onPress={navigateToPage} />
+    </SafeAreaView>
+  );
+}
+
+export default First;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+});
